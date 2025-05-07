9901bbdf03286a9a5c9da4493ea8e1f6b7c550c8
jaineelmodi11 <79771890+jaineelmodi11@users.noreply.github.com>
2025-05-06 23:55:46 -0400

    Initial Commit

---
+++ MovieMatch/MovieMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatchApp.swift
+//  MovieMatch
+//
+//  Created by Jaineel Modi on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieMatchTests/MovieMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatchTests.swift
+//  MovieMatchTests
+//
+//  Created by Jaineel Modi on 2025-05-06.
+//
+
+import Testing
+@testable import MovieMatch
+
+struct MovieMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
